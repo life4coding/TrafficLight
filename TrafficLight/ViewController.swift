a8b98fb35965f6ad3a7c845d3d7ffa4fc23491d8
kiwi <life4coding@gmail.com>
2017-06-16 20:38:35 +0000

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by kyx on 2017/6/16.
+//  Copyright © 2017年 DIFF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
